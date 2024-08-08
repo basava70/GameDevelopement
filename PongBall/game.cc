b601ff4a5ed3ca55fb73821bae01cdbe9b2179d6
SeshuHomePc <seshu.basava70@gmail.com>
2024-08-08 15:39:31 +0200

    Created PongBall folder

---
+++ PongBall/game.cc
@@ -0,0 +1,190 @@
+#include "game.h"
+#include "SDL3/SDL_rect.h"
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_keyboard.h>
+#include <SDL3/SDL_log.h>
+#include <SDL3/SDL_oldnames.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_scancode.h>
+#include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_timer.h>
+#include <cstdlib>
+
+bool Game::Initialize() {
+  int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+  if (sdlResult != 0) {
+    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+    return false;
+  }
+  Window_ = SDL_CreateWindow("Game Programming in C++, Chapter 1", WindowWidth_,
+                             WindowHeight_, 0);
+  if (!Window_) {
+    SDL_Log("Failed to create window %s", SDL_GetError());
+    return false;
+  }
+
+  Renderer_ = SDL_CreateRenderer(Window_, NULL);
+  if (!Renderer_) {
+    SDL_Log("Failed to create renderer %s", SDL_GetError());
+    return false;
+  }
+  return true;
+}
+
+void Game::ProcessInput() {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_EVENT_QUIT:
+      IsRunning_ = false;
+      break;
+    }
+  }
+
+  const Uint8 *state = SDL_GetKeyboardState(NULL);
+
+  if (state[SDL_SCANCODE_ESCAPE])
+    IsRunning_ = false;
+
+  PaddleDir_ = 0;
+
+  // move top if W is pressed, negative y direction
+  if (state[SDL_SCANCODE_W])
+    PaddleDir_ += -1;
+
+  // move bottom if S is pressed, positive y direction
+  if (state[SDL_SCANCODE_S])
+    PaddleDir_ += 1;
+}
+
+void Game::UpdateGame() {
+  // Wait until 8.33ms (120 FPS) has elapsed since last frame
+  while (!(SDL_GetTicks() - TicksCount_ >= 8)) {
+    ;
+  }
+
+  /**
+   * -- Delta time is the difference in ticks from last frame
+   * -- converted to seconds
+   **/
+  Uint64 currentTicks = SDL_GetTicks();
+  float deltaTime = (currentTicks - TicksCount_) / 1000.0f;
+
+  // Clamp maximum delta time to 0.05
+  if (deltaTime > 0.05f)
+    deltaTime = 0.05f;
+  TicksCount_ = SDL_GetTicks();
+
+  // move the paddle accordingly
+  // We are moving 300.0f pixels per second
+  if (PaddleDir_ != 0) {
+    PaddlePos_.y += PaddleDir_ * 600.0f * deltaTime;
+
+    if (PaddlePos_.y < PaddleHeight_ / 2.0f + Thickness_)
+      PaddlePos_.y = PaddleHeight_ / 2.0f + Thickness_;
+
+    if (PaddlePos_.y > WindowHeight_ - PaddleHeight_ / 2.0f - Thickness_)
+      PaddlePos_.y = WindowHeight_ - PaddleHeight_ / 2.0f - Thickness_;
+  }
+
+  // updating the ball
+  BallPos_.x += BallVel_.x * deltaTime;
+  BallPos_.y += BallVel_.y * deltaTime;
+
+  /**
+   * -- Collision detection
+   * -- For top wall, if BallPos_.y <= Thickness_, we have a hit in frame A
+   * -- Now, we change the direction of ball, BallVel_.y *= -1.
+   * -- However, in frame B, we might still be in collision zone, albeit
+   * -- in the correct direction, so we must add another condition
+   * -- to not get stuck in an infinite collision loop.
+   **/
+
+  // top wall collision
+  if (BallPos_.y <= Thickness_ * 3.0f / 2 && BallVel_.y < 0.0f)
+    BallVel_.y *= -1;
+
+  // right wall collision
+  if (BallPos_.x >= WindowWidth_ - Thickness_ * 3.0f / 2 && BallVel_.x > 0.0f)
+    BallVel_.x *= -1;
+
+  // bot wall collision
+  if (BallPos_.y >= WindowHeight_ - Thickness_ * 3.0f / 2 && BallVel_.y > 0.0f)
+    BallVel_.y *= -1;
+
+  // left wall collision **** game ends *****
+  if (BallPos_.x <= Thickness_ * 3.0f / 2 && BallVel_.x < 0.0f)
+    IsRunning_ = false;
+
+  /**
+   * -- Collision with the paddle
+   * -- We will check if the abs diff in y values is <= PaddleHeight_/2
+   * -- and diff in x values is <= Thickness_
+   * -- and BallVel_.x < 0 (ball is going left toward paddle)
+   **/
+  if (std::abs(BallPos_.y - PaddlePos_.y) <= PaddleHeight_ / 2 &&
+      std::abs(BallPos_.x - PaddlePos_.x) <= Thickness_ && BallVel_.x < 0.0f) {
+    BallVel_.x *= -1.0f;
+    Score_++;
+    DifficultyLevel_ += Score_;
+    BallVel_.x += DifficultyLevel_;
+  }
+}
+
+void Game::GenerateOutput() {
+  // set the entire screen to black and opacity to full 100%
+  SDL_SetRenderDrawColor(Renderer_, 0, 0, 0, 0);
+  // clear the back buffer
+  SDL_RenderClear(Renderer_);
+
+  // choosing black as the color for paddle and ball
+  SDL_SetRenderDrawColor(Renderer_, 255, 255, 255, 255);
+
+  // Creating walls
+  const SDL_FRect wall_top{0, 0, WindowWidth_, Thickness_};
+  const SDL_FRect wall_bot{0, WindowHeight_ - Thickness_, WindowWidth_,
+                           Thickness_};
+  const SDL_FRect wall_left{0, 0, Thickness_, WindowHeight_};
+  const SDL_FRect wall_right{WindowWidth_ - Thickness_, 0, Thickness_,
+                             WindowHeight_};
+  SDL_RenderFillRect(Renderer_, &wall_top);
+  SDL_RenderFillRect(Renderer_, &wall_bot);
+  SDL_RenderFillRect(Renderer_, &wall_left);
+  SDL_RenderFillRect(Renderer_, &wall_right);
+
+  /**
+   * -- Drawing the paddle
+   * -- Choosing green for paddle
+   **/
+  SDL_SetRenderDrawColor(Renderer_, 0, 255, 0, 255);
+  SDL_FRect paddle{PaddlePos_.x - Thickness_ / 2,
+                   PaddlePos_.y - PaddleHeight_ / 2, Thickness_, PaddleHeight_};
+  SDL_RenderFillRect(Renderer_, &paddle);
+
+  /**
+   * -- Drawing ball
+   * -- choosing red for ball
+   **/
+  SDL_SetRenderDrawColor(Renderer_, 255, 0, 0, 255);
+  SDL_FRect ball{BallPos_.x - Thickness_ / 2, BallPos_.y - Thickness_ / 2,
+                 Thickness_, Thickness_};
+  SDL_RenderFillRect(Renderer_, &ball);
+
+  // Finally, swapping the front and back buffers
+  SDL_RenderPresent(Renderer_);
+}
+
+void Game::ShutDown() {
+  SDL_DestroyRenderer(Renderer_);
+  SDL_DestroyWindow(Window_);
+  SDL_Quit();
+}
+
+void Game::RunLoop() {
+  while (IsRunning_) {
+    ProcessInput();
+    UpdateGame();
+    GenerateOutput();
+  }
+}
---
+++ PongBall/game.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_log.h>
+#include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_video.h>
+
+struct Vector2D {
+  float x;
+  float y;
+  Vector2D(float x_val, float y_val) : x(x_val), y(y_val) {}
+};
+
+class Game {
+public:
+  Game()
+      : IsRunning_(true), BallPos_(WindowWidth_ / 2, WindowHeight_ / 2),
+        PaddlePos_(Thickness_ * 3 / 2, WindowHeight_ / 2),
+        BallVel_(DifficultyLevel_ * -20, DifficultyLevel_ * 21) {}
+
+  // initialize the game
+  bool Initialize();
+
+  // runs the game loop, updates and takes keystrokes
+  void RunLoop();
+
+  // quits the game
+  void ShutDown();
+
+private:
+  /**
+   * -- part of the running loop
+   * -- first, we take input from keyboard/joystick/mouse
+   * -- second, we update the game
+   * -- third, we generate the output of the game
+   */
+  void ProcessInput();
+  void UpdateGame();
+  void GenerateOutput();
+
+  // Window crated by SDL
+  SDL_Window *Window_;
+  // bool variable to check if the game is running
+  bool IsRunning_;
+  SDL_Renderer *Renderer_;
+
+  const float WindowHeight_ = 768, WindowWidth_ = 1024, Thickness_ = 15;
+  int DifficultyLevel_ = 10;
+  const float PaddleHeight_ = DifficultyLevel_ * Thickness_;
+
+  Uint64 TicksCount_ = 0;
+
+  Vector2D PaddlePos_;
+  Vector2D BallPos_;
+  Vector2D BallVel_;
+
+  int PaddleDir_ = 0;
+  int Score_ = 0;
+};
